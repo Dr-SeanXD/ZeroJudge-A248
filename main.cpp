c5ed49c03279c89dfe65ca5d7fd9760bdd84d5bb
Dr-SeanXD <150778070+Dr-SeanXD@users.noreply.github.com>
2024-09-10 14:04:34 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(nullptr);
+    int a, b, N;
+    while (cin >> a >> b >> N)
+    {
+        cout << a/b << ".";
+        for (int i = 0; i<N; i++)
+        {
+            a %= b;
+            a *= 10;
+            cout << a/b;
+        }
+        cout << "\n";
+    }
+}
